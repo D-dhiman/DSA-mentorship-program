6f67055e93d50e9c44c25588b88be3547aa5a2ed
D-dhiman <144805020+D-dhiman@users.noreply.github.com>
2023-10-08 00:39:39 +0530

    Update 1_q4.cpp

--- 1_q4.cpp
+++ 1_q4.cpp
@@ -1,1 +1,21 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int n;
+    int i;
+    int j;
+    cout<<"enter the number:";
+    cin>>n;
+    cout<<2<<" ";
+    for (i=2;i<=n;i++){
+        for(j=2;j<=i;j++){
+            if (i%j==0){
+                break;
+            }else{
+                cout<<i<<" ";
+                break;
+            }
+        }
+    }
+}
