e9cdd2aeb37d4c04100c97d7ce8b6cbc28ccb28e
D-dhiman <144805020+D-dhiman@users.noreply.github.com>
2024-01-08 00:49:23 +0530

    Update 2_q2.cpp

--- 2_q2.cpp
+++ 2_q2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-
+void pattern2(int x){
+    for(int i=0;i<x;i++){
+        for(int j=0;j<x-i;j++){
+            cout<<"*"<< '\t';
+        }
+        cout<<'\n';
+    }
+}
 int main(){
     int t;
     cout<<"enter the number of rows :";
