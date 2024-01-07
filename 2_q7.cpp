d1e465bc4af9f8ecc69955126848f27fbc6b7c68
D-dhiman <144805020+D-dhiman@users.noreply.github.com>
2024-01-08 00:54:31 +0530

    Create 2_q7.cpp

---
+++ 2_q7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void pattern7(int x){
+    for(int i=1; i<=x ;i++){
+        for(int j=1; j<=i-1; j++)
+        {
+            cout<<'\t';
+        }
+        cout<<'*'<<'\n';
+    }
+}
+int main(){
+    int t;
+    cout<<"enter the number of rows :";
+    cin>>t;
+    pattern3(t);
+}
